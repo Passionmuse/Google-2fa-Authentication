37b411636dd571c67e1589c8052e86a3c4efc5b4
Passionmuse <passionmuse1216@gmail.com>
2024-06-12 14:20:56 +0900

    prepare all prequirement for implement google 2fa authentication

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use PragmaRX\Google2FA\Google2FA;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -37,4 +39,26 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
         $this->middleware('auth')->only('logout');
     }
+
+    public function authenticated(Request $request, $user)
+{
+    if ($user->uses_two_factor_auth) {
+        $google2fa = new Google2FA();
+
+        if ($request->session()->has('2fa_passed')) {
+            $request->session()->forget('2fa_passed');
+        }
+
+        $request->session()->put('2fa:user:id', $user->id);
+        $request->session()->put('2fa:auth:attempt', true);
+        $request->session()->put('2fa:auth:remember', $request->has('remember'));
+
+        $otp_secret = $user->google2fa_secret;
+        $one_time_password = $google2fa->getCurrentOtp($otp_secret);
+
+        return redirect()->route('2fa')->with('one_time_password', $one_time_password);
+    }
+
+    return redirect()->intended($this->redirectPath());
+}
 }
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/2fa', 'Auth\TwoFactorController@show')->name('2fa');
+Route::post('/2fa', 'Auth\TwoFactorController@verify')->name('2fa.verify');
